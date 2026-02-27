22d86ba72b14a30ca838dc4a2acd90beca8c80c7
SALEEM007-AI <saleemcr83@gmail.com>
2026-02-27 15:23:38 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,18 @@
 import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome To The Palindrome Checker Management System\nVersion : 1.0\nSystem intialized successfully.");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String text = sc.nextLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < text.length() / 2; i++) {
+            if (text.charAt(i) != text.charAt(text.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is it a Palindrome? : " + String.valueOf(isPalindrome).toLowerCase());
+
+        sc.close();
     }
 }
